0035eb4e9ac7f2b7fd74255e4489d6fa3affa7b3
nattyu <calvados441@gmail.com>
2024-05-01 12:45:58 +0900

    軽微な修正

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
 
 class Photo extends Model
 {
@@ -18,6 +17,6 @@ class Photo extends Model
 
     public function getPhotoUrlAttribute()
     {
-        return Storage::url('images/'. $this->filename);
+        return \Storage::url('images/'. $this->filename);
     }
 }
