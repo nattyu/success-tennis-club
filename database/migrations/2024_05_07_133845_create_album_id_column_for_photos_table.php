aceeff890220a0ef0ec31c10c7d65d379299519c
nattyu <calvados441@gmail.com>
2024-05-07 15:20:54 +0900

    migrationファイルの修正

--- database/migrations/2024_05_07_133845_create_album_id_column_for_photos_table.php
+++ database/migrations/2024_05_07_133845_create_album_id_column_for_photos_table.php
@@ -9,14 +9,21 @@ return new class extends Migration
     public function up()
     {
         Schema::table('photos', function (Blueprint $table) {
+            // album_id列を追加
+            $table->unsignedBigInteger('album_id')->after('user_id');
+
             // 新しい外部キー制約を追加
-            $table->foreign('album_id')->references('id')->on('galleries')->onDelete('cascade')->after('user_id');
+            $table->foreign('album_id')->references('id')->on('galleries')->onDelete('cascade');
         });
     }
 
     public function down()
     {
         Schema::table('photos', function (Blueprint $table) {
+            // 外部キー制約を削除する
+            $table->dropForeign(['album_id']);
+            
+            // album_id列を削除する
             $table->dropColumn('album_id');
         });
     }
